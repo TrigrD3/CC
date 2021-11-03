80519ede659a3944105eccbbc680e08985d664f1
SeanRcd <vincentius.sean@gmail.com>
2021-11-03 22:45:32 +0800

    kehapus

---
+++ index.php
@@ -0,0 +1,98 @@
+<?php 
+
+session_start();
+
+if(!isset($_SESSION["login"])){
+  header("Location: login.php");
+  exit;
+}
+
+include "head.php";
+require "config.php";
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+
+    <!-- ajax -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous" />
+
+    <!-- My CSS -->
+    <link rel="stylesheet" href="assets/Style.css" />
+
+    <!-- google font -->
+    <link rel="stylesheet" type="text/css" href="//fonts.googleapis.com/css?family=Open+Sans" />
+
+    <!-- font awesome -->
+    <link rel="stylesheet" href="path/to/font-awesome/css/font-awesome.min.css" />
+
+    <!-- Bootstrap Icons -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css" />
+    <title>Home</title>
+  </head>
+  <body>
+    <!-- most viewed -->
+    <section>
+<div class="most-viewed">
+  <div class="container">
+    <h1>Most Viewed</h1>
+    <hr>
+    <!-- perulangan untuk menampilkan data dari database -->
+    
+    <div class="row">
+    <?php foreach ($komik as $row) : ?>
+      <div class="col">
+      <div class="card" style="width: 18rem;">
+      <a href=""><?php echo '<img src="data:image/jpeg;base64,'.base64_encode( $row['cover'] ).'" width= "250px" height="370px" class="card-img-top"/>';?></a>
+  <div class="card-body">
+    <h5 class="card-title"><?= $row['judul']; ?> </h5>
+  </div>
+  <ul class="list-group list-group-flush">
+    <li class="list-group-item">
+      <a href="#" class="card-link"><?= $row['synopsis']; ?></a> </li>
+      <li class="list-group-item">
+      <a href="#" class="card-link"><?= $row['chapter']; ?></a> </li>
+  </ul>
+</div>
+    </div>
+    <?php endforeach; ?>
+  </div>
+</div>
+    </section>
+    <!-- akhir most viewed -->
+
+    <!-- new released -->
+    <section>
+<div class="new-released">
+  <div class="container">
+    <h1>New Released</h1>
+    <hr>
+    <div class="row">
+    <?php foreach ($komik as $row) : ?>
+      <div class="col">
+      <div class="card" style="width: 18rem;">
+      <a href=""><?php echo '<img src="data:image/jpeg;base64,'.base64_encode( $row['cover'] ).'" width= "250px" height="370px" class="card-img-top"/>';?></a>
+  <div class="card-body">
+    <h5 class="card-title"><?= $row['judul']; ?> </h5>
+  </div>
+  <ul class="list-group list-group-flush">
+    <li class="list-group-item">
+      <a href="#" class="card-link"><?= $row['synopsis']; ?></a> </li>
+      <li class="list-group-item">
+      <a href="#" class="card-link"><?= $row['chapter']; ?></a> </li>
+  </ul>
+</div>
+    </div>
+    <?php endforeach; ?>
+  </div>
+</div>
+    </section>
+
+  </body>
+</html>
