ef7f4d994df296957822f4d22592a68336350548
Jeremy Antonio <jerz.antonio10@gmail.com>
2021-12-10 02:26:44 +0800

    Fix about us

--- aboutus.php
+++ aboutus.php
@@ -1,4 +1,4 @@
-<?php 
+d<?php 
 require "config.php";
 include "head.php";
 
@@ -24,92 +24,150 @@ include "head.php";
 
     <!-- font awesome -->
     <link rel="stylesheet" href="path/to/font-awesome/css/font-awesome.min.css" />
+    <script src="https://kit.fontawesome.com/b70bdea85f.js" crossorigin="anonymous"></script>
 
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
     <!-- Bootstrap Icons -->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css" />
     <title>Home</title>
   </head>
   <body>
-      <section class="aboutus">
-          <div class="container">
-          <div class="row text-center ">
-          <div class="col">
-              <h2 style="color: aliceblue;"><b>Honbox is a website that allow the user to read comics online developed by: </b></h2>
-            
-            <p></p>
-          </div>
-        </div>
-          <div class="row text-center mb-3 ">
-              <div class="col fs-6 text-center">
-                <div class="card" style="width: 18rem;">
-                <img src="img/profile/arya.jpg" class="card-img-top" alt="..." ">
-                <div class="card-body">
-                    <p class="card-text">Muhammad Jatmika Aryaputra</p>
-                    <p class="card-text">email: 1910817210004@mhs.ulm.ac.id</p>
-                </div>
-                </div>
-              </div>
-
-              <div class="col-md-4 fs-6 text-center">
-                <div class="card" style="width: 18rem;">
-                <img src="img/profile/aldo.jpeg" class="card-img-top" alt="...">
-                <div class="card-body">
-                    <p class="card-text">Aldo Savero</p>
-                    <p class="card-text">email: 1910817310007@mhs.ulm.ac.id</p>
-                </div>
-                </div>
-              </div>
-              
-              <div class="col-md-4 fs-6 text-center">
-                <div class="card" style="width: 18rem;">
-                <img src="img/profile/jessica.jpg" class="card-img-top" alt="...">
-                <div class="card-body">
-                    <p class="card-text">Eugynia Jessica Virgynia Rahman</p>
-                    <p class="card-text">email: 1910817220013@mhs.ulm.ac.id</p>
-                </div>
-                </div>
-              </div>
-          </div>
-          <div class="row text-center ">
-          <div class="col-md-4 fs-6 text-center">
-                <div class="card" style="width: 18rem;">
-                <img src="img/profile/jeremy.jpg" class="card-img-top" alt="...">
-                <div class="card-body">
-                    <p class="card-text">Jeremy Antonio</p>
-                    <p class="card-text">email: 1910817310011@mhs.ulm.ac.id</p>
-                </div>
-                </div>
-              </div>
-
-              <div class="col-md-4 fs-6 text-center">
-                <div class="card" style="width: 18rem;">
-                <img src="img/profile/razaq.jpeg" class="card-img-top" alt="...">
-                <div class="card-body">
-                    <p class="card-text">Muhammad Razaq Lazuardi</p>
-                    <p class="card-text">email: 1910817310010@mhs.ulm.ac.id</p>
-                </div>
-                </div>
-              </div>
-
-              <div class="col-md-4 fs-6 text-center">
-                <div class="card" style="width: 18rem;">
-                <img src="img/profile/sean.jpg" class="card-img-top" alt="...">
-                <div class="card-body">
-                    <p class="card-text">Sean Richard Sindunata</p>
-                    <p class="card-text">email: 1910817210020@mhs.ulm.ac.id</p>
-                </div>
-                </div>
-              </div>
+      <section class="container">
+          <div class="row">
+            <div class="col-md-12">
+              <h2 class="text-white text-center">Meet the Team behind the Project HonBox</h2>
             </div>
-            </div>
-          </div>
-          </div>
-          
+              <div class="col lg-3 col-md-4 col-sm-3 my-2">
+                <div class="shadow 
+                d-flex 
+                justify-content-center 
+                align-items-center p-3 bg-dark rounded-lg flex-column" >
+                  <div class="person-img">
+                    <img src="img/profile/arya.jpg" class="img-fluid rounded-circle" alt="...">
+                  </div>
+                  <div class="person-name my-2">
+                    <h3 class="text-white">Arya</h3>
+                  </div>
+                  <div class="info">
+                    <h6 class="text-white">Project Manager</h6>
+                  </div>
+                  <div class="social-icons">
+                    <a href="https://github.com/TrigrD3" class="text-white"><i class="fa fa-github p-2" aria-hidden="true"></i></a>
+                    <a href="https://www.youtube.com/channel/UCsRLMFvJwl-kFha4LGfDPMA" class="text-white"><i class="fa fa-youtube p-2" aria-hidden="true"></i></a>
+                    <a href="https://www.instagram.com/aryaputra132/" class="text-white"><i class="fa fa-instagram p-2" aria-hidden="true"></i></a>
+                  </div>
+                </div>
+              </div>
 
+              <div class="col lg-3 col-md-4 col-sm-3 my-2">
+                <div class="shadow 
+                d-flex 
+                justify-content-center 
+                align-items-center p-3 bg-dark rounded-lg flex-column" >
+                  <div class="person-img">
+                    <img src="img/profile/aldo.jpeg" class="img-fluid rounded-circle" alt="...">
+                  </div>
+                  <div class="person-name my-2">
+                    <h3 class="text-white">Aldo</h3>
+                  </div>
+                  <div class="info">
+                    <h6 class="text-white">Anggota</h6>
+                  </div>
+                  <div class="social-icons">
+                    <a href="https://github.com/kingchetah" class="text-white"><i class="fa fa-github p-2" aria-hidden="true"></i></a>
+                    <a href="https://www.youtube.com/channel/UC9rJ8y3_gwG-YiW1DpuPa2w" class="text-white"><i class="fa fa-youtube p-2" aria-hidden="true"></i></a>
+                    <a href="https://www.instagram.com/aldosvr_/" class="text-white"><i class="fa fa-instagram p-2" aria-hidden="true"></i></a>
+                  </div>
+                </div>
+              </div>
 
-        
-      </section>
+              <div class="col lg-3 col-md-4 col-sm-3 my-2">
+                <div class="shadow 
+                d-flex 
+                justify-content-center 
+                align-items-center p-3 bg-dark rounded-lg flex-column" >
+                  <div class="person-img">
+                    <img src="img/profile/jessica.jpg" class="img-fluid rounded-circle" alt="...">
+                  </div>
+                  <div class="person-name my-2">
+                    <h3 class="text-white">Jessica</h3>
+                  </div>
+                  <div class="info">
+                    <h6 class="text-white">Anggota</h6>
+                  </div>
+                  <div class="social-icons">
+                    <a href="https://github.com/eugyniajcv" class="text-white"><i class="fa fa-github p-2" aria-hidden="true"></i></a>
+                    <a href="https://www.youtube.com/channel/UCBVol6qiX_rRSN9U3GkLM4A" class="text-white"><i class="fa fa-youtube p-2" aria-hidden="true"></i></a>
+                    <a href="https://www.instagram.com/eugyniajc/" class="text-white"><i class="fa fa-instagram p-2" aria-hidden="true"></i></a>
+                  </div>
+                </div>
+              </div>
 
+              <div class="col lg-3 col-md-4 col-sm-3 my-2">
+                <div class="shadow 
+                d-flex 
+                justify-content-center 
+                align-items-center p-3 bg-dark rounded-lg flex-column" >
+                  <div class="person-img">
+                    <img src="img/profile/jeremy.jpg" class="img-fluid rounded-circle" alt="...">
+                  </div>
+                  <div class="person-name my-2">
+                    <h3 class="text-white">Jeremy</h3>
+                  </div>
+                  <div class="info">
+                    <h6 class="text-white">Anggota</h6>
+                  </div>
+                  <div class="social-icons">
+                    <a href="https://github.com/fully-coded" class="text-white"><i class="fa fa-github p-2" aria-hidden="true"></i></a>
+                    <a href="https://www.youtube.com/channel/UCZh3dTi8bS0inCGMqRiJwsQ" class="text-white"><i class="fa fa-youtube p-2" aria-hidden="true"></i></a>
+                    <a href="https://www.instagram.com/jeremy.a.o/" class="text-white"><i class="fa fa-instagram p-2" aria-hidden="true"></i></a>
+                  </div>
+                </div>
+              </div>
 
+              <div class="col lg-3 col-md-4 col-sm-3 my-2">
+                <div class="shadow 
+                d-flex 
+                justify-content-center 
+                align-items-center p-3 bg-dark rounded-lg flex-column" >
+                  <div class="person-img">
+                    <img src="img/profile/razaq.jpeg" class="img-fluid rounded-circle" alt="...">
+                  </div>
+                  <div class="person-name my-2">
+                    <h3 class="text-white">Razaq</h3>
+                  </div>
+                  <div class="info">
+                    <h6 class="text-white">Anggota</h6>
+                  </div>
+                  <div class="social-icons">
+                    <a href="https://github.com/Ospirav" class="text-white"><i class="fa fa-github p-2" aria-hidden="true"></i></a>
+                    <a href="https://www.youtube.com/channel/UCAqa_iWsxGmjhruer4Qc9tg" class="text-white"><i class="fa fa-youtube p-2" aria-hidden="true"></i></a>
+                    <a href="https://www.instagram.com/zlaz69/" class="text-white"><i class="fa fa-instagram p-2" aria-hidden="true"></i></a>
+                  </div>
+                </div>
+              </div>
+              <div class="col lg-3 col-md-4 col-sm-3 my-2">
+                <div class="shadow 
+                d-flex 
+                justify-content-center 
+                align-items-center p-3 bg-dark rounded-lg flex-column" >
+                  <div class="person-img">
+                    <img src="img/profile/sean.jpg" class="img-fluid rounded-circle" alt="...">
+                  </div>
+                  <div class="person-name my-2">
+                    <h3 class="text-white">Sean</h3>
+                  </div>
+                  <div class="info">
+                    <h6 class="text-white">Anggota</h6>
+                  </div>
+                  <div class="social-icons">
+                    <a href="https://github.com/seanrcd" class="text-white"><i class="fa fa-github p-2" aria-hidden="true"></i></a>
+                    <a href="https://www.youtube.com/channel/UCEKrbM9HRkeiWEQHVdKvIYw" class="text-white"><i class="fa fa-youtube p-2" aria-hidden="true"></i></a>
+                    <a href="https://www.instagram.com/karas_muha/" class="text-white"><i class="fa fa-instagram p-2" aria-hidden="true"></i></a>
+                  </div>
+                </div>
+              </div>
+            </div>          
+    </section>
   </body>
 </html>
