fb9c921d454500d2cc1671535d165186c6121f05
TrigrD3 <TrigrD3@users.noreply.github.com>
2021-12-09 18:18:57 +0800

    about us

---
+++ aboutus.php
@@ -0,0 +1,114 @@
+<?php 
+require "config.php";
+include "head.php";
+
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+
+    <!-- ajax -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous" />
+
+    <!-- My CSS -->
+    <link rel="stylesheet" href="assets/Style.css" />
+
+    <!-- google font -->
+    <link rel="stylesheet" type="text/css" href="//fonts.googleapis.com/css?family=Open+Sans" />
+
+    <!-- font awesome -->
+    <link rel="stylesheet" href="path/to/font-awesome/css/font-awesome.min.css" />
+
+    <!-- Bootstrap Icons -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css" />
+    <title>Home</title>
+  </head>
+  <body>
+      <section>
+          <div class="container">
+          <div class="row text-center mb-3">
+          <div class="col">
+            <h2 style="color: aliceblue;">Honbox is a website that allow the user to read comics online developed by: </h2>
+            <p></p>
+          </div>
+        </div>
+          <div class="row text-center mb-3 ">
+              <div class="col-md-4 fs-6 text-center">
+                <div class="card" style="width: 18rem;">
+                <img src="img/profile/arya.jpg" class="card-img-top" alt="..." ">
+                <div class="card-body">
+                    <p class="card-text">Nama: Muhammad Jatmika Aryaputra</p>
+                    <p class="card-text">email: 1910817210004@mhs.ulm.ac.id</p>
+                </div>
+                </div>
+              </div>
+
+              <div class="col-md-4 fs-6 text-center">
+                <div class="card" style="width: 18rem;">
+                <img src="img/profile/aldo.jpeg" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <p class="card-text">Nama: Aldo Savero</p>
+                    <p class="card-text">email: 1910817310007@mhs.ulm.ac.id</p>
+                </div>
+                </div>
+              </div>
+              
+              <div class="col-md-4 fs-6 text-center">
+                <div class="card" style="width: 18rem;">
+                <img src="img/profile/jessica.jpg" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <p class="card-text">Nama: Eugynia Jessica Virgynia Rahman</p>
+                    <p class="card-text">email: 1910817220013@mhs.ulm.ac.id</p>
+                </div>
+                </div>
+              </div>
+          </div>
+          <div class="row text-center mb-3">
+          <div class="col-md-4 fs-6 text-center">
+                <div class="card" style="width: 18rem;">
+                <img src="img/profile/jeremy.jpg" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <p class="card-text">Nama: Jeremy Antonio</p>
+                    <p class="card-text">email: 1910817310011@mhs.ulm.ac.id</p>
+                </div>
+                </div>
+              </div>
+
+              <div class="col-md-4 fs-6 text-center">
+                <div class="card" style="width: 18rem;">
+                <img src="img/profile/razaq.jpeg" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <p class="card-text">Nama: Muhammad Razaq Lazuardi</p>
+                    <p class="card-text">email: 1910817310010@mhs.ulm.ac.id</p>
+                </div>
+                </div>
+              </div>
+
+              <div class="col-md-4 fs-6 text-center">
+                <div class="card" style="width: 18rem;">
+                <img src="img/profile/sean.jpg" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <p class="card-text">Nama: Sean Richard Sindunata</p>
+                    <p class="card-text">email: 1910817210020@mhs.ulm.ac.id</p>
+                </div>
+                </div>
+              </div>
+            </div>
+            </div>
+          </div>
+          </div>
+          
+
+
+        
+      </section>
+
+
+  </body>
+</html>
--- index.php
+++ index.php
@@ -104,6 +104,10 @@ $trending=mysqli_query($conn,"SELECT * FROM komik ORDER BY rating DESC");
     </div>
     </section>
   <!-- akhir new released -->
+<section>
 
+<a href="aboutus.php"><button type="button" class="btn text-center" style="color: aliceblue; ">About Us</button></a>
+
+</section>
   </body>
 </html>
